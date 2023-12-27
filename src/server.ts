6ba8a20e0640a26e95c46e515fba03feb75c3d76
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-12-27 23:26:33 +0530

    Feat: /api product route configured

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 const PORT = 3001;
@@ -7,6 +8,8 @@ app.get("/", (req, res) => {
   res.status(200);
   res.send("Hello");
 });
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
