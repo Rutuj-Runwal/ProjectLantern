261d8f2f1f90da064e70fb5e517c688608e57699
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-12-28 00:41:20 +0530

    Chore: Json body-parser and cors middleware initialized

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import router from "./router";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200);
