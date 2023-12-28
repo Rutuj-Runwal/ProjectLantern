92dd38f2955f83e746c0ec9eaa861ffaec35fa86
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-12-28 17:08:01 +0530

    Feat: Jwt verfiy middleware created

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -3,7 +3,27 @@ import jwt from "jsonwebtoken";
 export const createJWT = (user: any) => {
   const token = jwt.sign(
     { username: user.username, id: user.id },
-    process.env.JWT_SEC as string
+    process.env.JWT_SECRET as string
   );
   return token;
+};
+
+// Middleware to verfiy user JWT
+export const protect = (req: any, res: any, next: any) => {
+  // Get Authorization Bearer
+  const bearer = req.headers.authorization;
+
+  const token = bearer.split(" ")[1]; // Get JWT token: "Bearer token"
+
+  if (!token) {
+    res.status(401).json({ message: "Not Authorized" });
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = payload;
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "Not Authorized" });
+  }
 };
